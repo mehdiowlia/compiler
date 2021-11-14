bea2e9551ae7022b81eaae618c7f2510b02cc376
mehdiowlia <76652206+mehdiowlia@users.noreply.github.com>
2021-11-14 21:37:13 +0330

    Create enum.cpp

---
+++ enum.cpp
@@ -0,0 +1,23 @@
+//In The Name Of God
+//work with enums
+
+#include<iostream>
+
+using namespace std;
+//define enum:
+enum colors{red,yellow,green,brown};
+enum days{sun,mon,tue,wen,thu,fri,sat};
+int main()
+{
+
+    cout<<"red code : "<<red<<endl;
+    cout<<"wednesday : "<<wen<<endl;
+
+    return 0;
+}
+
+/*
+output:
+red code : 0
+wednesday : 3
+*/
