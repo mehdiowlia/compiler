497b97314083ff507df5f7b6f527d06e05bdb57a
mehdiowlia <76652206+mehdiowlia@users.noreply.github.com>
2021-11-26 17:25:47 +0330

    Update starting scanner.cpp

--- starting scanner.cpp
+++ starting scanner.cpp
@@ -19,6 +19,174 @@ Todo:
 
 using namespace std;
 
+///////////////////////////////////Node Class/////////////////////////////
+class Node
+{
+
+
+public:
+
+	string name;
+	Node* info;
+	Node* next;
+
+	//4 constructors:
+	//1
+	Node()
+	{
+		this->name = "";
+		this->info = NULL;
+		this->next = NULL;
+
+	}
+	//2    
+	Node(string name)
+	{
+		this->name = name;
+		this->info = NULL;
+		this->next = NULL;
+	}
+	//3
+	Node(string name, Node* info)
+	{
+		this->name = name;
+		this->info = info;
+		this->next = NULL;
+
+	}
+	//4
+	Node(string name, Node* info, Node* next)
+	{
+		this->name = name;
+		this->info = info;
+		this->next = next;
+	}
+
+};
+
+/////////////////////////////////////LinkedList Class/////////////////////////////////////////////
+
+class LinkedList
+{
+public:
+
+	Node* head;
+	//constructor :
+	LinkedList()
+	{
+		head = NULL;
+	}
+	//function for search a node :
+	bool searchNode(string name)
+	{
+		Node* temp = head;
+
+		while (temp != NULL)
+		{
+			if (temp->name == name)
+			{
+				return true;
+			}
+			temp = temp->next;
+		}
+
+		return false;
+
+	}
+	//function for insert new node to linked list :
+	void insertNode(string name, Node* info = NULL)
+	{
+		if (searchNode(name))
+		{
+			return;
+		}
+
+		Node* newNode = new Node(name);
+
+		if (head == NULL)
+		{
+			head = newNode;
+			return;
+		}
+
+		Node* temp = head;
+
+		while (temp->next != NULL)
+		{
+
+			temp = temp->next;
+		}
+
+
+		temp->next = newNode;
+
+
+	}
+	//function for print all the nodes :
+	void printList()
+	{
+		if (head == NULL)
+		{
+			cout << "linked list is empty !" << endl;
+			return;
+		}
+
+		Node* temp = head;
+
+		while (temp != NULL)
+		{
+			cout << temp->name << "---->";
+			temp = temp->next;
+		}
+		cout <<"NULL"<< endl;
+
+
+	}
+
+};
+
+////////////////////////////Hash Table////////////////////////////////
+
+class HastTable
+{
+	private:
+
+		LinkedList table[8];
+
+	public:
+
+		//function for calculate hash of identifire's name and append it to table : 
+		int hashing(string identifire)
+		{
+			int len = identifire.length();//length of input
+			char chArr[256];//char array for casting
+			strcpy(chArr, identifire.c_str());//copy string to char array
+			int sum = 0;
+
+			for (int i = 0; i < len; i++)
+			{
+				sum += int(chArr[i]);//add ascii code of char's to each other
+			}
+
+			int index = sum % 8;
+			table[index].insertNode(identifire);
+			return index;
+
+		}
+		//function to print Hash Table :
+		void printTable()
+		{
+			cout << "symbole table : \n\n";
+			for (int i = 0; i < 8; i++)
+			{
+				cout << "symbole raw " << i + 1 << " : ";
+				table[i].printList();
+				cout<<endl;
+			}
+		}
+
+};
+
 ///////////////////////Scanner Class//////////////////////////
 class ScannerClass
 {
@@ -28,7 +196,7 @@ class ScannerClass
 		//fstream for work with files
 		fstream my_file;
 		int file_pointer;
-
+		//enum reserved_word {_program_,_begin_,_end_,_var_,_integer_,_function_};
 	public:
 
 		//class constructor
@@ -56,7 +224,7 @@ class ScannerClass
 			while (true) 
 			{	
 				
-				if (end_fo_file())
+				if (end_of_file())
 				{
 					return "";
 				}
@@ -84,7 +252,7 @@ class ScannerClass
 					else
 					{	
 						//check for file is end ? or not
-						if (end_fo_file())
+						if (end_of_file())
 						{
 							return "";
 						}
@@ -101,13 +269,25 @@ class ScannerClass
 			
 		}
 
-		bool reserved_words_detector()
+		/*
+		int hash_string(string str1)
 		{
-			return false;
-		}
+			string str=word_detector(str1);
 
-		
-		bool end_fo_file()
+			if(str=="program")
+				return _program_;
+			if(str=="begin")
+				return _begin_;
+			if(str=="end")
+				return _end_;
+			if(str=="var")
+				return _var_;
+			else
+				return -1;
+		}
+		*/
+
+		bool end_of_file()
 		{
 			//eof() ==> true if arrive end of the file
 			if (my_file.eof())
@@ -124,23 +304,45 @@ class ScannerClass
 int main()
 {	
 	
-	string file_name = "/home/cloud/programming/test/test1.txt";
+	string file_name = "/home/cloud/programming/test/test4.txt";
+
 	ScannerClass hi;
-	//🚫all_str should be linked list to become onlimited🚫//
-	string all_str[100];
-	for(int i=0;!hi.end_fo_file();i++)
+	LinkedList all_str;
+
+	//string all_str[100];
+	for(int i=0;!hi.end_of_file();i++)
 	{
 		string str=hi.word_detector(file_name);
-		all_str[i]=str;
-		cout<<str<<endl;
+		all_str.insertNode(str);
+		//cout<<str<<endl;
 	}
 
 	
-	for(int i=0;i<=99;i++)
+
+
+
+	HastTable t1;
+	Node* ntemp;
+	ntemp=all_str.head;
+	for(int i=0;(i<=99) && (ntemp->next!=NULL);i++)
 	{
-		cout<<"str "<<i+1<<" : "<<all_str[i]<<endl;
+		//cout<<"str "<<i+1<<" : "<<all_str[i]<<endl;
+		t1.hashing(ntemp->name);
+		ntemp=ntemp->next;
+
 	}
 
+/*
+
+	t1.hashing("position");
+	t1.hashing("rate");
+	t1.hashing("initial");
+	t1.hashing("positino");//"positno" hash id equal to position
+	t1.hashing("position");//if compiler arrive to repetitive identifire
+
+*/
+
+	t1.printTable();
 
 	return 0;  
 
